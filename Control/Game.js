6c2827ad6bf2eac71dc795a9c1eb3578d89ce05e
Matias-Notebook <otaku372@gmail.com>
2024-09-06 17:05:30 -0300

    6/9 notas

---
+++ Control/Game.js
@@ -0,0 +1,12 @@
+//generar codigo
+// debe tener la clase objeto personaje con los atributos principales
+// cargar todas las imagenes y despues mostrar cada escenario cuando se deba.
+// crear funcion para comienzo de juego
+// funcion de cambio de escenario
+// funcion de filtrado de escenario. (luz o no)
+// funcion de de tiempo de juego
+// BGM
+// efectos en el cambio de escenario
+// efectos al recibir daño (shake + red glow)
+// funcion de morir (muerte)
+// mostrar el resultado final (ganar o perder)
